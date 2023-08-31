e370aecd311082afd6bae951bc9540585fd8505d
Nippon <adamayyad1999@hotmail.com>
2023-08-31 20:54:51 +0300

    veux: Build libloc_net_iface
    Signed-off-by: Nippon <adamayyad1999@hotmail.com>

--- device.mk
+++ device.mk
@@ -293,7 +293,9 @@ PRODUCT_PACKAGES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor
+    android.system.net.netd@1.1.vendor \
+    libloc_net_iface \
+    libloc_net_iface.vendor
 
 # Neural Networks
 PRODUCT_PACKAGES += \
