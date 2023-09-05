89fd7c47cfa2be3a6262caf75fbe6a0a321ae9ff
alexandrusemina <alexandrusemina8@gmail.com>
2023-09-05 13:13:36 +0300

    veux: Define overlay-spark and add support for battery health

--- device.mk
+++ device.mk
@@ -328,7 +328,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
-
+    $(LOCAL_PATH)/overlay-spark
+    
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayVeux \
     DialerOverlayVeux \
