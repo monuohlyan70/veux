b32df774025b5d8037bb52b7015b74e4d9c140c0
alexandrusemina <alexandrusemina8@gmail.com>
2023-10-14 13:22:23 +0300

    veux: vendorsetup: remove display repo and clone hw/xiaomi from evox

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,4 @@ git clone https://github.com/nnippon99/device_xiaomi_veux-kernel device/xiaomi/v
 echo "Cloning Vendor..."
 git clone https://gitlab.com/nnippon99/vendor_xiaomi_veux vendor/xiaomi/veux
 echo "Cloning hardware related stuff..."
-cd hardware/qcom-caf/sm8350 && rm -rf display && git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-sm8350 display && cd display && git remote add f https://github.com/xiaomi-sm6375-devs/android_hardware_qcom-caf_sm8350_display && git fetch f && git cherry-pick 27ccd6fe9b9a7fdceff422c0f15d1abc52bc7410 && cd ../../../..
+git clone https://github.com/Evolution-X/hardware_xiaomi hardware/xiaomi
