06e092cbd410ae750cf3372e7de777e2e22d4140
alexandrusemina <alexandrusemina8@gmail.com>
2023-10-14 13:38:45 +0300

    veux: vendorsetup: switch to personal vendor repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,6 @@ echo "Cloning kernel..."
 git clone https://github.com/crdroidandroid/android_kernel_oneplus_sm8350 kernel/xiaomi/sm6375 --depth=1
 git clone https://github.com/nnippon99/device_xiaomi_veux-kernel device/xiaomi/veux-kernel --depth=1
 echo "Cloning Vendor..."
-git clone https://gitlab.com/nnippon99/vendor_xiaomi_veux vendor/xiaomi/veux
+git clone https://gitlab.com/alexandrusemina/vendor_xiaomi_veux vendor/xiaomi/veux
 echo "Cloning hardware related stuff..."
 git clone https://github.com/Evolution-X/hardware_xiaomi hardware/xiaomi
