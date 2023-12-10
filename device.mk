bf84b9f8fee4e8f6ab9ceff54029b48d6192b88c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-10 15:35:22 +0530

    veux: Build and enable AvoidAppsInCutoutOverlay
    Allows showing statusbar with a black background to hide the notch,
    similar to the option in stock MIUI. It works, but we still need to
    define proper rounded corners for it to look nice.
    
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>
    Signed-off-by: aswinop <itsaswinop@gmail.com>

--- device.mk
+++ device.mk
@@ -334,6 +334,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
+    AvoidAppsInCutoutOverlay \
     CarrierConfigOverlayVeux \
     DialerOverlayVeux \
     FrameworksResOverlayVeux \
