7fa7f35437f22cea3ee58127b27b9dbf6d080c1a
aswinop <itsaswinop@gmail.com>
2024-01-28 19:48:12 +0530

    veux: Switch to Rashoumon kernel
    Signed-off-by: aswinop <itsaswinop@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ echo "Cloning kernel..."
 git clone https://github.com/alexandrusemina/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
 
 echo "Cloning Vendor..."
-git clone https://gitlab.com/alexandrusemina/vendor_xiaomi_veux vendor/xiaomi/veux
+git clone https://gitlab.com/aswinop/vendor_xiaomi_veux vendor/xiaomi/veux
 
 echo "Cloning hardware related stuff..."
 git clone https://github.com/Evolution-X/hardware_xiaomi hardware/xiaomi
