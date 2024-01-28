a553a78b84d1edcfdee71fb1d3c31d3314bc01e9
aswinop <itsaswinop@gmail.com>
2024-01-28 19:50:36 +0530

    veux: Include GCam
    Signed-off-by: aswinop <itsaswinop@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,6 @@ git clone https://gitlab.com/aswinop/vendor_xiaomi_veux vendor/xiaomi/veux
 
 echo "Cloning hardware related stuff..."
 git clone https://github.com/Evolution-X/hardware_xiaomi hardware/xiaomi
+
+echo "Cloning Gcam stuff..."
+git clone https://github.com/AswinOP/vendor_xiaomi_veux-gcam vendor/xiaomi/veux-gcam
